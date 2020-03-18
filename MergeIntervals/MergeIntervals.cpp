43c26e9d5e42a6dfe946ad2bf2828d6d76b3b7c6
ujjwalgupta99 <gupta.ujjwal515@gmail.com>
2020-03-18 16:01:52 +0000

    [LocalTest]:[MergeIntervals]:[c++]

--- MergeIntervals/MergeIntervals.cpp
+++ MergeIntervals/MergeIntervals.cpp
@@ -8,9 +8,31 @@ class MergeIntervals {
 public:
 	// Implement your solution by completing the below function
 	vector<vector<int>> merge(vector<vector<int>>& intervals) {
-	vector<vector<int>> res;
-
-        return res;
+	//Sort to make traversing easier
+	sort(intervals.begin(),intervals.end());
+    
+	    vector<vector<int>> result;
+    
+		//this is how you traverse a matrix 
+	    for(unsigned int i=1;i<=intervals.size();i++){
+            vector<int> insidearray;
+    
+			//Overlap Found like : (1,3) (2,5)
+	        if(i<intervals.size() && intervals[i-1][1] >= intervals[i][0]){
+               
+			    intervals[i][0] = min(intervals[i-1][0],intervals[i][0]);
+                intervals[i][1] = max(intervals[i-1][1], intervals[i][1]);
+            }
+    
+			//No overlap found simply push the array to the matrix
+	        else{
+                insidearray.push_back(intervals[i-1][0]);
+                insidearray.push_back(intervals[i-1][1]);
+                result.push_back(insidearray);
+            }
+        }
+       
+	    return result;
     }
 };
 
