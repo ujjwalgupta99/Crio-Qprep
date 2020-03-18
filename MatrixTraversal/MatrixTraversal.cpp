afae43db3d87681f1c3e245525a39b804e04cef3
ujjwalgupta99 <gupta.ujjwal515@gmail.com>
2020-03-18 16:18:50 +0000

    [LocalTest]:[MatrixTraversal]:[c++]

--- MatrixTraversal/MatrixTraversal.cpp
+++ MatrixTraversal/MatrixTraversal.cpp
@@ -9,7 +9,39 @@ public:
     // Implement your solution by completing the below function	
     vector<int> traversalPath(vector<vector<int>>& matrix, int currPosX, int currPosY, int dirToMove, int stepsToMove) {
         vector<int> res;
-	
+
+		while(stepsToMove--){
+				switch ((dirToMove))
+				{
+					//right 
+					case 1:
+						currPosY++;
+					break;
+				
+					//Down 
+					case 2:
+						currPosX++;
+					break;
+
+					//Left 
+					case 3:
+						currPosY--;
+					break;
+
+					//Up 
+					case 4:
+						currPosX--;
+					break;
+				}
+			//												that's how you find matrix dimensions
+			if(currPosX<0 || currPosY<0 || currPosX>=int(matrix.size()) || currPosY>=int(matrix[0].size())){
+				//Need to clear vector otherwise previous results would reflect
+				res.clear();
+				res.push_back(-1);
+				return res;
+			}
+			res.push_back(matrix[currPosX][currPosY]);
+		}
         return res;
     }
 };
