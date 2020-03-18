7884781c846d93dca4c0931688b3e9d51418461f
ujjwalgupta99 <gupta.ujjwal515@gmail.com>
2020-03-18 18:48:55 +0000

    [LocalTest]:[NumberOfIslands]:[c++]

--- NumberOfIslands/NumberOfIslands.cpp
+++ NumberOfIslands/NumberOfIslands.cpp
@@ -1,4 +1,4 @@
- #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "../crio/cpp/io/FastIO.hpp"
 #include "../crio/cpp/io/ReadMatrix.hpp"
 #include "../crio/cpp/io/PrintMatrix.hpp"
@@ -8,9 +8,39 @@ class NumberOfIslands {
 public:
     // Implement your solution by completing the below function
     int numIslands(vector<string>& grid) {
-        int x = 0;
+        //int x = 0;
+		if(grid.empty()){
+            return 0;
+        }
+        int res = 0;
+		//Row
+        int m = grid.size();
+        //Column
+		int n = grid[0].size();
+        //O(m*n) : Complexity
+		for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j]=='1'){
+                    res++;
+                    fill(grid,i,j);
+                }
+            }
+        }
+        return res;
+        //return x;
+    }
 
-        return x;
+	void fill(vector<string>& grid, int i, int j){
+        if(i >= 0 && i < (int)grid.size() && j >= 0 && j < (int)grid[0].size()){
+            if(grid[i][j] == '1'){
+                grid[i][j] = 'x';
+				//fill the surrounding recursively with anything but 1 ('x' because it looks like minesweeper)
+                fill(grid,i+1,j);
+                fill(grid,i-1,j);
+                fill(grid,i,j+1);
+                fill(grid,i,j-1);
+            }
+        }
     }
 };
 
